5e89a97b45e0be4cd21d2a1038df9c6c8d2e1893
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-09-20 22:42:26 +0300

    Add basic solution

--- reverse_polish_notation.rb
+++ reverse_polish_notation.rb
@@ -1,4 +1,65 @@
-def number?(string)
+operator_order = {
+  "(" => 0,
+  ")" => 0,
+  "+" => 1,
+  "-" => 1,
+  "*" => 2,
+  "/" => 2,
+}
+
+def operand?(string)
   string.match? /^[0-9.]+$/
 end
 
+def operator?(string)
+  string.match? /^[+\-*\/]$/
+end
+
+if ARGV.length < 1
+  puts "You need to separate operators and operands with a space"
+  return
+end
+
+operators = []
+postfix = []
+
+ARGV.each do |entity|
+  if operand? entity
+    postfix << entity
+
+    next
+  end
+
+  if operator? entity
+    if operators.length > 1
+      until operator_order[operators[-1]] < operator_order[entity] do
+        postfix << operators[-1]
+        operators.pop
+      end
+    end
+
+    operators << entity
+    next
+  end
+
+  if entity == "("
+    operators << entity
+    next
+  end
+
+  if entity == ")"
+    until operators[-1] == "(" do
+      postfix << operators[-1]
+      operators.pop
+    end
+    next
+  end
+
+  puts entity
+  puts "The given text is not a valid infix expression"
+  return
+end
+
+operators.reject! { |operator| operator == "(" }
+
+puts "#{postfix.join " "} #{operators.join " "}"
