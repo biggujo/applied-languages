13bdcdb8263dd3393650a458ea4ddb3f940bb168
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-09-27 20:32:07 +0300

    Add complete implementation

--- main.rb
+++ main.rb
@@ -1,37 +1,70 @@
 # 1. "Abstract" class
-class Worker
+class Processor
   def execute
     raise "Can not run a method from an abstract class"
   end
 end
 
 # 2. Concrete implementation
-class ConcreteWorker < Worker
-  attr_accessor :string
+class ConcreteProcessor < Processor
+  attr_accessor :value
 
-  def initialize(string)
-    @string = string;
+  def initialize(value)
+    @value = value
   end
 
   def execute
-    @string.upcase!
+    @value
   end
 end
 
-# 3. "Abstract" decorator class
-class Decorator < Worker
-  attr_accessor :worker
+# 3. Decorator class does nothing as additional work
+class Decorator < Processor
+  attr_accessor :processor
 
-  def initialize(worker)
-    @worker = worker
+  def initialize(processor)
+    @processor = processor
   end
 
   def execute
-    @worker.execute
+    @processor.execute
+    @processor
   end
 end
 
-concrete_worker = ConcreteWorker.new("someString")
-decorator = Decorator.new(concrete_worker)
+# 4. Decorator classes do some actions
+class AppendDecorator < Decorator
+  def initialize(decorator, text)
+    super(decorator.processor)
+    @text = text
+  end
 
-puts decorator.execute
+  def execute
+    @processor.value += @text
+    @processor
+  end
+end
+
+class ReverseDecorator < Decorator
+  def initialize(decorator)
+    super(decorator.processor)
+  end
+
+  def execute
+    @processor.value.reverse!
+    @processor
+  end
+end
+
+# Result: someString
+processor = Decorator.new(ConcreteProcessor.new("someString"))
+
+# Result: someStringsomeAppendedString
+processor = AppendDecorator.new(processor, "someAppendedString")
+processor.execute
+
+# Result: gnirtSdedneppAemosgnirtSemos
+processor = ReverseDecorator.new(processor)
+processor.execute
+
+puts processor.processor.value
