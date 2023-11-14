713a8ef3608bdce6fb4f97703faa0366af4af8bd
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-11-14 15:41:45 +0200

    Add complete parser and output data

--- main.rb
+++ main.rb
@@ -1,10 +1,28 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
-WEBSITE = "https://www.hospitalsafetygrade.org/all-hospitals"
+def parse_hospitals(output_file_name)
+  url = "https://www.hospitalsafetygrade.org/all-hospitals"
 
-hospitals_document = Nokogiri::HTML(URI.open(WEBSITE))
+  website_page = URI.open(url)
+  document = Nokogiri::HTML(website_page)
 
-hospitals_document.css('#BlinkDBContent_849210 > ul > li > a').each do |link|
-  puts link.content
+  hospital_names = document.css('#BlinkDBContent_849210 > ul > li > a')
+
+  if hospital_names.length == 0
+    print "No hospital names has been found"
+  end
+
+  CSV.open(output_file_name, "w") do |csv|
+    hospital_names.each_with_index do |link, index|
+      csv << [index + 1, link]
+    end
+  end
+end
+
+begin
+  parse_hospitals("hospitals.csv")
+rescue SocketError => error
+  puts error.message
 end
