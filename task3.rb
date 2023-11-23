5ab5e33f77ebdb66e740f2ab3b9f0ae4c6edff2f
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-11-23 11:32:27 +0200

    Add tasks

---
+++ task3.rb
@@ -0,0 +1,32 @@
+def word_count(text)
+  # Remove all non-letter and non-number characters
+  filtered_text = text.gsub(/[^a-zA-Z0-9 ]/, " ")
+
+  counter = {}
+
+  filtered_text.split(" ").each do |word|
+    # puts word
+    if counter[word] == nil
+      counter[word] = 1
+    else
+      counter[word] += 1
+    end
+  end
+
+  counter
+end
+
+def print_results(results)
+  results.each do |word, count|
+    puts "#{word}: #{count}"
+  end
+end
+
+text = File.read "text.txt"
+
+results = word_count text
+
+results = results.sort_by { |word, count| count }
+results.reverse!
+
+print_results results
